fd2c52dac287a61f9ab1f72f03ca557d0670f293
Naruto X <92075412+ItzNaruto@users.noreply.github.com>
2024-07-23 22:15:47 +0530

    .

--- Kora/__main__.py
+++ Kora/__main__.py
@@ -1,15 +1,6 @@
-import asyncio
-from pyrogram import idle
 from Kora import app
 
-async def main():
-    try:
-        await app.send_message(6495253163, "**I'm Started Successfully ✨**")
-        print("Client Started!")
-    except Exception as e:
-        print(f"Error : {e}")
-    await idle()
-
-if __name__ == "__main__" :
-    asyncio.run(main())
-    LOGGER.info("Client Stopped!")
+if __name__ == "__main__":
+     app.run()
+     with app:
+        app.send_message(6495253163, "**I'm Successfully Started ✨**")
