5f9bb83a575a812c0251c40366377fff8e837106
Naruto X <92075412+ItzNaruto@users.noreply.github.com>
2024-07-23 22:13:30 +0530

    .

--- Kora/__init__.py
+++ Kora/__init__.py
@@ -1,35 +1,35 @@
-import os
-import logging
 import time
-from pyrogram import Client, filters, errors
+import logging
+from pyrogram import Client
 
-# Time
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+                                                  logging.StreamHandler()], format="[KORA]: %(message)s")
 StartTime = time.time()
 
-# Logging
-logging.basicConfig(level=logging.INFO)
-LOGGER = logging.getLogger(__name__)
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
 
-# Pyro Client
 app = Client(
-  "Kora", 
-  api_id=API_ID, 
-  api_hash=API_HASH, 
-  bot_token=BOT_TOKEN
+    "Kora", 
+    api_id=API_ID, 
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Kora/Plugins")
 )
-
-# Pyro Start
-app.start()
-
-# Vars
-API_ID = 18641799
-API_HASH = "2027706706fd39baf84c01ff5b95a6a6"
-BOT_TOKEN = "7001280039:AAE1Zm4aDgrOcr2L9_pbivf6J7TBE0SJfWU"
-BOT_ID = app.me.id
-BOT_NAME = app.me.first_name
-BOT_USERNAME = app.me.username
-
-# Logs
-print(f"[{BOT_USERNAME}] Is Starting. | Kora Project")
-print(f"[{BOT_USERNAME}] Project Maintained By Kora")
-print(f"[{BOT_USERNAME}] Is Alive")
